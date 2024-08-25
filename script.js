864c70ccb764945fe3f42531d4fbef67f5ab844c
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-25 10:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+function addListItems(count) {
+	for(let i = 0; i < count; i++)
+		{
+	const listItem = document.createElement("li");
+	listItem.textContent = `Item ${i + 1}`;
+	list.appendChild(listItem);
+	}
+}
+
+addListItems(10);
